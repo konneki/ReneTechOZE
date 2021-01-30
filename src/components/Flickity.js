e9390801cd4ae8d5b133567f03d2cbd08edd348e
Daniel Konnek <konnekdaniel@gmail.com>
2021-01-30 13:33:45 +0100

    add react-flickity to make carousel of sponsors

---
+++ src/components/Flickity.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/Flickity.css';
+import Flickity from 'react-flickity-component';
+
+const Flickity = () => {
+  return <div></div>;
+};
+
+export default Flickity;
